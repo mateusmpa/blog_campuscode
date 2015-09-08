d4adb6f86ad4088d97a6fa1abb2d2ffc27b04997
Mateus Alves <mateus.alves@locaweb.com.br>
2015-09-08 10:15:30 -0300

    blog campus code - estudo

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,17 @@
+class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+
+    if @post.comments.create(comment_params)
+      redirect_to @post, notice: 'Comment was successfully created'
+    else
+      redirect_to @post, alert: 'Error creating comment'
+    end
+  end
+
+  private
+  # Never trust parameters from the scary internet, only allow the white list through.
+    def comment_params
+      params.require(:comment).permit(:author, :body)
+    end
+end
